cda7031f0c3ddddb434260951c352944123a4fb9
Dante DaCapo <nathan-savina@hotmail.fr>
2020-11-19 12:03:42 +0100

    too much stuff

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 import pickle
 from collections import OrderedDict
 import graphviz
-from sklearn.model_selection import train_test_split, cross_val_score, StratifiedShuffleSplit, cross_val_predict
+import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedShuffleSplit, cross_val_predict, RandomizedSearchCV, validation_curve, GridSearchCV
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.metrics import classification_report, confusion_matrix, explained_variance_score
+from sklearn.metrics import classification_report, confusion_matrix, explained_variance_score, accuracy_score
 from sklearn.decomposition import PCA
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor, VotingClassifier, ExtraTreesClassifier, GradientBoostingClassifier
@@ -228,24 +229,68 @@ def evaluate_model(path_file_train):
     print(scores)
     print(f"Average Protoss : {avg/len(scores)}")
     """
-
+    """
     ids = data_tool.get_numbers_ids_reference(path_file_train)
     for index, player in enumerate(y):
         y[index] = ids[player]
+    """
+    # print(y)
+    # classifier = RandomForestRegressor()
 
-    print(y)
-    classifier = RandomForestRegressor()
-    # classifier = RandomForestClassifier()
+    # Create the random grid
+    n_estimators = [10, 100, 300, 500, 700]
+    max_features = ['auto', 'sqrt']
+    bootstrap = [True, False]
+    max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+    min_samples_split = [2, 5, 10]
+    min_samples_leaf = [1, 2, 4]
+    criterion = ['gini', 'entropy'] 
+    random_grid = {
+        'n_estimators': n_estimators,
+        'max_features': max_features,
+        'max_depth': max_depth,
+        'min_samples_split': min_samples_split,
+        'min_samples_leaf': min_samples_leaf,
+        'bootstrap': bootstrap,
+        'criterion': criterion,
+    }
+    # Find the best parameters
+    classifier = RandomForestClassifier(
+        n_estimators= 300,
+        min_samples_split= 2,
+        min_samples_leaf= 1,
+        max_features= 'sqrt',
+        max_depth= 50,
+        criterion= 'entropy',
+        bootstrap= False
+    )
+    classifier_random = RandomizedSearchCV(estimator=classifier, param_distributions=random_grid, n_iter=50, cv=3, verbose=2, random_state=42, n_jobs=-1)
+    classifier_random.fit(X, y)
+    print(classifier_random.best_params_)
+    # Test
+    best_classifier = classifier_random.best_estimator_
+    print("Read csv test data...")
+    data = data_tool.read_csv("starcraft-2-player-prediction-challenge-2020/TEST.CSV")
+    print("Get Features test data ...")
+    features = data_tool.get_features(data, train=False)
+
+    print("Evaluate results...")
+    with open("Results/test.csv", 'w') as sub_file:
+        sub_file.write("RowId,prediction\n")
+        y_pred = best_classifier.predict(features)
+        print("Store results...")
+        for i, res in enumerate(y_pred):
+            sub_file.write(f"{i+1},{res}\n") 
     # classifier = GradientBoostingClassifier()
     # classifier = RandomForestClassifier(bootstrap=False)
-
+    """
     scores = cross_val_score(classifier, X, y, cv=5)
     avg = 0
     for s in scores:
         avg += s
     print(scores)
     print(f"Average : {avg/len(scores)}")
-
+    """
     """
     X_train, X_test, y_train, y_test = train_test_split(features, y, test_size=0.20)
 
@@ -258,15 +303,6 @@ def evaluate_model(path_file_train):
     # print(confusion_matrix(y_test, y_pred))
     print(classification_report(y_test, y_pred))
     # print(explained_variance_score(y_test, y_pred))
-    """
-    """
-    sss = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
-    for train_index, test_index in sss.split(X, y):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        classifier.fit(X_train, y_train)
-        y_pred = classifier.predict(X_test)
-        print(confusion_matrix(y_test, y_pred))
     """
 
 
@@ -311,7 +347,16 @@ def train_and_make_predictions(path_file_train: str, path_file_test: str, namefi
     # classifier = RandomForestClassifier()
     # classifier = RandomForestClassifier(class_weight="balanced")
 
-    classifier = GradientBoostingClassifier()
+    classifier = RandomForestClassifier(
+        n_estimators= 300,
+        min_samples_split= 2,
+        min_samples_leaf= 1,
+        max_features= 'sqrt',
+        max_depth= 50,
+        criterion= 'entropy',
+        bootstrap= True,
+        oob_score=True
+    )
     classifier.fit(X, y)
 
     print("Read csv test data...")
@@ -430,25 +475,119 @@ def random_forest_experiements(path_file_train: str):
     
 
     # TODO Faire une fonction linéaire, pas un bar plot
-    plt.bar(results_n_estimators_x, results_n_estimators_y, width=10, color='r', align='edge')
+    plt.plot(results_n_estimators_x, results_n_estimators_y)
     plt.show()
+
+
+def evaluate_model_2(path_file_train: str):
+    print("Read csv ...")
+    data = data_tool.read_csv(path_file_train)
+    print("Get Features ...")
+    X = data_tool.get_features(data, train=True)
+
+    print("Testing model...")
+    y = []
+    for elem in X:
+        y.append(elem.pop(0))
+
+    classifier = RandomForestClassifier()
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=40) 
+
+
+    # fit the model
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+    # make predictions
+    y_pred = model.predict(X_test)
+    # evaluate predictions
+    acc = accuracy_score(y_test, y_pred)
+    print('Accuracy standard params: %.3f' % acc)
+
+    """
+    # GridSearch
+    param_grid = {
+        'bootstrap': [True],
+        'max_depth': [30, 40, 50, 60],
+        'max_features': [20, 30, 40],
+        'min_samples_leaf': [1, 2, 3, 4],
+        'min_samples_split': [8, 10, 12],
+        'n_estimators': [500, 600, 800, 1000]
+    }
+    rf = RandomForestClassifier()
+    grid_search = GridSearchCV(
+        estimator = rf,
+        param_grid = param_grid,
+        cv = 3,
+        n_jobs = -1,
+        verbose = 2
+    )
+
+    # => Result: {'bootstrap': True, 'max_depth': 60, 'max_features': 20, 'min_samples_leaf': 1, 'min_samples_split': 8, 'n_estimators': 600}
+    grid_search.fit(X_train, y_train)
+    print(grid_search.best_params_)
+
+    best_grid = grid_search.best_estimator_
+    """
+    model2 = RandomForestClassifier(
+        bootstrap=True,
+        max_depth = 60,
+        max_features = 20,
+        min_samples_leaf = 1,
+        min_samples_split = 8,
+        n_estimators = 600
+    )
+    model2.fit(X_train, y_train)
+    # make predictions
+    y_pred = model2.predict(X_test)
+    # evaluate predictions
+    acc = accuracy_score(y_test, y_pred)
+    print('Accuracy best params : %.3f' % acc)
+
+
+    # En utilisant la méthode ci-dessous
+    # n_estimators = 1000
+    # max_depth = 40
+    # max_features = 40
+    """
+    num_est = range(1, 60, 5)
+    train_scoreNum, test_scoreNum = validation_curve(
+                                RandomForestClassifier(n_estimators=500, max_depth=40, min_samples_leaf=1),
+                                X = X_train, y = y_train, 
+                                param_name = 'max_features', 
+                                param_range = num_est, cv = 3)
+
+    scores = classification_report(y_test, y_pred)
+    print(scores)
+    
+    # Plot mean accuracy scores for training and test sets
+    plt.plot(num_est, test_scoreNum, label="Cross-validation score", color="blue")
+
+    # Create plot
+    plt.title("Validation Curve With Random Forest")
+    plt.xlabel("Number Of Trees")
+    plt.ylabel("Accuracy Score")
+    plt.tight_layout()
+    plt.legend(loc="best")
+    plt.show()
+    """
+
 
 if __name__ == "__main__":
     """
     train_and_make_predictions(
         "starcraft-2-player-prediction-challenge-2020/TRAIN.CSV",
         "starcraft-2-player-prediction-challenge-2020/TEST.CSV",
-        "Results/26_SUBMISSION.CSV"
+        "Results/30_SUBMISSION.CSV"
     )
-
     train_and_make_predictions_three_classifiers(
         "starcraft-2-player-prediction-challenge-2020/TRAIN.CSV",
         "starcraft-2-player-prediction-challenge-2020/TEST.CSV",
         "Results/27_SUBMISSION.CSV"
     )
     """
-    # evaluate_model("starcraft-2-player-prediction-challenge-2020/TRAIN.CSV")
+    evaluate_model_2("starcraft-2-player-prediction-challenge-2020/TRAIN.CSV")
     # data_tool.get_informations_data("starcraft-2-player-prediction-challenge-2020/TRAIN.CSV")
-    random_forest_experiements("starcraft-2-player-prediction-challenge-2020/TRAIN.CSV")
+    # random_forest_experiements("starcraft-2-player-prediction-challenge-2020/TRAIN.CSV")
     print("Done")
 
